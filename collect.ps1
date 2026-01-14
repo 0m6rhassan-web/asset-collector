442bfcebac3468bbe7d3fd650c7f8a29df7b1bb0
0m6rhassan-web <0m6rhassan@gmail.com>
2026-01-14 23:49:16 +0200

    Refactor disk size and battery capacity retrieval
    Updated disk size retrieval to use Win32_DiskDrive for total size and added error handling for battery capacity retrieval.

--- collect.ps1
+++ collect.ps1
@@ -10,11 +10,13 @@ if(!$C){$C=$CF}
 
 $R=(Get-CimInstance Win32_PhysicalMemory | % {[math]::Round($_.Capacity/1GB)}) -join '+'
 
-# هنا ناخد **حجم كل قسم فقط بالجيجابايت**، بدون أي DeviceID
+# تعديل الهاردسك لجلب الحجم الصافي فقط وإضافة GB
 try {
-    $SSD=(Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" |
-        ForEach-Object { [math]::Round($_.Size/1GB) }
-    ) -join '+'
+    # استخدمنا Win32_DiskDrive لأنه يقرأ الهارد ككل وليس البارتيشنات
+    $SSD = (Get-CimInstance Win32_DiskDrive | ForEach-Object {
+        $Size = [math]::Round($_.Size/1GB)
+        "{0}GB" -f $Size
+    }) -join '+'
     if(!$SSD){$SSD='N/A'}
 }catch{$SSD='N/A'}
 
@@ -22,16 +24,15 @@ $V=(Get-CimInstance Win32_VideoController | ?{$_.Name -notmatch 'Intel'} | % Nam
 if(!$V){$V=(Get-CimInstance Win32_VideoController)[0].Name}
 
 try{
-    $bat=Get-WmiObject -Namespace root/WMI -Class BatteryFullChargedCapacity
-    $des=Get-WmiObject -Namespace root/WMI -Class BatteryStaticData
+    $bat=Get-WmiObject -Namespace root/WMI -Class BatteryFullChargedCapacity -ErrorAction SilentlyContinue
+    $des=Get-WmiObject -Namespace root/WMI -Class BatteryStaticData -ErrorAction SilentlyContinue
     if($bat -and $des){
-        $H='{0}%' -f([math]::Round(($bat.FullChargedCapacity/$des.DesignedCapacity)*100))
+        $H='{0}%' -f([math]::Round(($bat[0].FullChargedCapacity/$des[0].DesignedCapacity)*100))
     } else {
         $b2=Get-CimInstance Win32_Battery
         if($b2){$H=$b2.EstimatedChargeRemaining+'%'}else{$H='N/A'}
     }
 }catch{$H='N/A'}
 
-# إرسال النتائج
 "$S,$M,$C,$R,$SSD,$V,$H" |
 Invoke-RestMethod -Uri $HOOK -Method Post -ContentType "text/plain"
