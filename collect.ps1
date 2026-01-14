a07ad3432a86f82035c65a6c45871b8305adeaeb
0m6rhassan-web <0m6rhassan@gmail.com>
2026-01-14 23:33:33 +0200

    Refactor data sending to webhook in collect.ps1
    Updated the method for sending data to the webhook by combining the CSV string creation and the Invoke-RestMethod call into one line.

--- collect.ps1
+++ collect.ps1
@@ -10,7 +10,10 @@ if(!$C){$C=$CF}
 
 $R=(Get-CimInstance Win32_PhysicalMemory|%{[math]::Round($_.Capacity/1GB)})-join'+'
 
-try{$D=(Get-PhysicalDisk|%{'{0}GB-{1}'-f([math]::Round($_.Size/1GB)),$_.MediaType})-join'+'}catch{$D='N/A'}
+try{
+$D=(Get-CimInstance Win32_DiskDrive |
+%{'{0}GB-{1}' -f ([math]::Round($_.Size/1GB)),$_.Model}) -join '+'
+}catch{$D='N/A'}
 
 $V=(Get-CimInstance Win32_VideoController|?{$_.Name-notmatch'Intel'}|% Name)-join' / '
 if(!$V){$V=(Get-CimInstance Win32_VideoController)[0].Name}
@@ -21,6 +24,6 @@ $d=Get-WmiObject -Namespace root/WMI -Class BatteryStaticData
 if($b -and $d){$H='{0}%' -f([math]::Round(($b.FullChargedCapacity/$d.DesignedCapacity)*100))}else{$H='N/A'}
 }catch{$H='N/A'}
 
-$CSV="$S,$M,$C,$R,$D,$V,$H"
+"$S,$M,$C,$R,$D,$V,$H" |
+Invoke-RestMethod -Uri $HOOK -Method Post -ContentType "text/plain"
 
-Invoke-RestMethod -Uri $HOOK -Method Post -Body $CSV -ContentType "text/plain"
