6121ab723a9503448ccd1210c83fb51adcac1486
0m6rhassan-web <0m6rhassan@gmail.com>
2026-01-14 23:51:35 +0200

    v1
    Updated disk retrieval logic to exclude USB drives and added error handling for battery information.

--- collect.ps1
+++ collect.ps1
@@ -10,29 +10,34 @@ if(!$C){$C=$CF}
 
 $R=(Get-CimInstance Win32_PhysicalMemory | % {[math]::Round($_.Capacity/1GB)}) -join '+'
 
-# تعديل الهاردسك لجلب الحجم الصافي فقط وإضافة GB
+# استخراج الهاردسك الداخلي فقط وتجاهل الـ USB
 try {
-    # استخدمنا Win32_DiskDrive لأنه يقرأ الهارد ككل وليس البارتيشنات
-    $SSD = (Get-CimInstance Win32_DiskDrive | ForEach-Object {
+    # استخدمنا Get-PhysicalDisk مع فلتر يمنع الـ USB (BusType -neq 'USB')
+    $Disks = Get-PhysicalDisk | Where-Object { $_.BusType -ne 'USB' -and $_.Size -gt 0 }
+    $SSD = ($Disks | ForEach-Object {
         $Size = [math]::Round($_.Size/1GB)
         "{0}GB" -f $Size
     }) -join '+'
     if(!$SSD){$SSD='N/A'}
-}catch{$SSD='N/A'}
+}catch{
+    # في حال فشل الأمر السابق، نستخدم بديل أقدم مع فلتر للـ Media Type
+    $SSD = (Get-CimInstance Win32_DiskDrive | Where-Object { $_.InterfaceType -ne 'USB' } | ForEach-Object {
+        "{0}GB" -f [math]::Round($_.Size/1GB)
+    }) -join '+'
+}
 
 $V=(Get-CimInstance Win32_VideoController | ?{$_.Name -notmatch 'Intel'} | % Name) -join ' / '
 if(!$V){$V=(Get-CimInstance Win32_VideoController)[0].Name}
 
-try{
-    $bat=Get-WmiObject -Namespace root/WMI -Class BatteryFullChargedCapacity -ErrorAction SilentlyContinue
-    $des=Get-WmiObject -Namespace root/WMI -Class BatteryStaticData -ErrorAction SilentlyContinue
-    if($bat -and $des){
-        $H='{0}%' -f([math]::Round(($bat[0].FullChargedCapacity/$des[0].DesignedCapacity)*100))
-    } else {
-        $b2=Get-CimInstance Win32_Battery
-        if($b2){$H=$b2.EstimatedChargeRemaining+'%'}else{$H='N/A'}
-    }
-}catch{$H='N/A'}
+# كود البطارية (رقم صافي)
+try {
+    $bat = Get-WmiObject -Namespace root/WMI -Class BatteryFullChargedCapacity
+    $des = Get-WmiObject -Namespace root/WMI -Class BatteryStaticData
+    if ($bat -and $des) {
+        $H = '{0}%' -f ([math]::Round(($bat[0].FullChargedCapacity / $des[0].DesignedCapacity) * 100))
+    } else { $H = 'N/A' }
+} catch { $H = 'N/A' }
 
+# إرسال النتائج
 "$S,$M,$C,$R,$SSD,$V,$H" |
 Invoke-RestMethod -Uri $HOOK -Method Post -ContentType "text/plain"
